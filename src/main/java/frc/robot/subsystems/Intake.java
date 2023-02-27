ce1123f8870e11d63b04c39a6534af0a2e1f8c4a
Agent33OfChrist <artandbirds@gmail.com>
2023-02-27 17:40:07 -0500

    Added stuff

---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,28 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+public class Intake extends SubsystemBase
+{
+    private CANSparkMax intake;
+
+    public Intake()
+    {
+        intake = new CANSparkMax(7, MotorType.kBrushless);
+        intake.setIdleMode(IdleMode.kCoast);
+
+    }
+
+    public void setIntake(double value)
+    {
+        intake.set(value);
+    }
+
+
+
+}
